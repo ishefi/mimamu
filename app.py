611e37539795b1fd3e0a0e26e8a2a5fd9c6625c8
Itamar Shefi <itamar@3bears.ai>
2023-01-29 18:31:18 +0200

    choose date

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
-import datetime
+from datetime import datetime
+import os
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from routers import routers
 from session import get_mongo
 
+
+def get_date():
+    try:
+        date = datetime.strptime(os.environ.get("GAME_DATE", ""), '%Y-%m-%d')
+        return date.date()
+    except ValueError:
+        return datetime.utcnow().date()
+
+
 app = FastAPI()
 app.state.mongo = get_mongo()
-app.state.date = datetime.datetime.utcnow().date()
+app.state.date = get_date()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
