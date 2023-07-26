d83264e2f07d33b353a05764e18a65cfe3ba7f93
Itamar Shefi <itamar@3bears.ai>
2023-07-26 11:02:40 +0300

    Allow changing prompt in script

--- scripts/set_riddle.py
+++ scripts/set_riddle.py
@@ -79,16 +79,28 @@ def main():
             img.show()
         riddle = schemas.GameData(picture=image_url, words=prompt_words, author=author)
         logic = RiddleLogic(mongo_riddles=mongo, date=date)
-        redacted = riddle.copy(deep=True)
-        logic.redact(redacted)
-        print(f"{author}'s Prompt: {prompt}")
-        print(f"Redacted: {' '.join(redacted.words)}")
-        if input("Is ok? [yN] ") in ["y", "Y"]:
-            logic.set_riddle(riddle, force=args.force)
-            date = get_date(mongo)
+        _approve_riddle(logic, riddle, prompt, args.force)
+        date = get_date(mongo)
         url = input(f"doing {date}\nNew URL > ")
     print("done")
 
+
+def _approve_riddle(logic, riddle, prompt, force):
+    redacted = riddle.copy(deep=True)
+    logic.redact(redacted)
+    print(f"{riddle.author}'s Prompt: {prompt}")
+    print(f"Redacted: {' '.join(redacted.words)}")
+    is_ok = input("Is ok? [cyN] ")
+    if is_ok in ["y", "Y"]:
+        logic.set_riddle(riddle, force=force)
+    elif is_ok in ["c", "C"]:
+        new_prompt = input("new prompt words? ")
+        riddle = schemas.GameData(
+            picture=riddle.picture,
+            words=new_prompt.replace("-", " ").split(),
+            author=riddle.author,
+        )
+        _approve_riddle(logic, riddle, new_prompt, force)
 
 if __name__ == "__main__":
     main()
