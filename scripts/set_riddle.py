74f52a2d3748d3c1604c80c84fa4ebeb58c269e2
Itamar Shefi <itamarshefi@gmail.com>
2024-03-02 12:09:45 +0200

    Print Hebrew properly

--- scripts/set_riddle.py
+++ scripts/set_riddle.py
@@ -120,17 +120,17 @@ def main():
             picture=image_url, words=prompt_words, author=author, dalle=dalle
         )
         logic = RiddleLogic(mongo_riddles=mongo, date=date, lang=args.lang)
-        _approve_riddle(logic, riddle, prompt, args.force)
+        _approve_riddle(logic, riddle, prompt, args.force, args.lang)
         date = get_date(mongo[args.lang])
         url = input(f"doing {date}\nNew URL > ")
     print("done")
 
 
-def _approve_riddle(logic, riddle, prompt, force):
+def _approve_riddle(logic, riddle, prompt, force, lang):
     redacted = riddle.copy(deep=True)
     logic.redact(redacted)
-    print(f"{riddle.author}'s Prompt: {prompt}")
-    print(f"Redacted: {' '.join(redacted.words)}")
+    print(f"{riddle.author}'s Prompt: {prompt if lang == 'en' else prompt[::-1]}")
+    print(f"Redacted: {' '.join(redacted.words if lang == 'en' else reversed([word[::-1] for word in redacted.words]))}")
     is_ok = input("Is ok? [cyN] ")
     if is_ok in ["y", "Y"]:
         logic.set_riddle(riddle, force=force)
@@ -141,7 +141,7 @@ def _approve_riddle(logic, riddle, prompt, force):
             words=new_prompt.replace("-", " ").split(),
             author=riddle.author,
         )
-        _approve_riddle(logic, riddle, new_prompt, force)
+        _approve_riddle(logic, riddle, new_prompt, force, lang)
 
 
 if __name__ == "__main__":
