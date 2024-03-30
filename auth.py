70ff1d8d54d4f9b74c5c424ce9e3c0b8f10a846b
Itamar Shefi <itamarshefi@gmail.com>
2024-03-30 18:44:31 +0300

    Add some security for bad token

--- auth.py
+++ auth.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+import random
+import time
+
 from fastapi import Header
 from fastapi import HTTPException
 from fastapi import Request
@@ -9,4 +12,5 @@ def verify_token(
     request: Request, x_mmm_token: str | None = Header(default=None)
 ) -> None:
     if x_mmm_token is None or x_mmm_token != request.app.state.secret_token:
+        time.sleep(random.random() * 2)
         raise HTTPException(status.HTTP_403_FORBIDDEN)
