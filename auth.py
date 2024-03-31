2f1f8b47d27a4e847bee355faef15a0680b9b60c
Itamar Shefi <itamarshefi@gmail.com>
2024-03-31 18:13:45 +0300

    Allow authenticating by query param

--- auth.py
+++ auth.py
@@ -9,7 +9,18 @@ from fastapi import status
 from common import config
 
 
-def verify_token(x_mmm_token: str | None = Header(default=None)) -> None:
-    if x_mmm_token is None or x_mmm_token != config.SECRET_TOKEN:
+def verify_token(
+    x_mmm_token: str | None = Header(default=None), mmm_token: str | None = None
+) -> None:
+    if not _verify(x_mmm_token) and not _verify(mmm_token):
         time.sleep(random.random() * 2)
         raise HTTPException(status.HTTP_403_FORBIDDEN)
+
+
+def _verify(mmm_token: str | None) -> bool:
+    if mmm_token is None:
+        return False
+    elif mmm_token != config.SECRET_TOKEN:
+        return False
+    else:
+        return True
